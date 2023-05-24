2ca04cbdbbea0ad02445bfe9f4a575b18874bcb0
Carl Sundvisson <carltham@hotmail.co.uk>
2023-05-24 19:15:37 +0200

    First version Testable with JUnit

---
+++ src/main/java/com/mycompany/bookapi/BookAPIImpl.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.bookapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *
+ * @author carl
+ */
+@SpringBootApplication
+public class BookAPI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello " + BookAPI.class.getSimpleName() + "!");
+    }
+}
