dcea5a8727ab01fbc000aecbe767dbba6e4079c6
Carl Sundvisson <carltham@hotmail.co.uk>
2023-05-25 19:08:20 +0200

    Test Cleanup

--- src/test/java/com/mycompany/bookapi/pub/BookAPITest.java
+++ src/test/java/com/mycompany/bookapi/pub/BookAPITest.java
@@ -5,8 +5,10 @@ import com.mycompany.bookapi.dto.BookDTO;
 import com.mycompany.bookapi.repository.FakeDB;
 import com.mycompany.bookapi.test.TestBooks;
 import com.mycompany.bookapi.test.TestConstants;
+import java.net.URISyntaxException;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,44 +44,79 @@ public class BookAPITest {
     }
 
     @Test
-    public void testAll() {
+    public void testCreate() {
 
-        List<Book> result = instance.list();
-        List<Book> expResult = FakeDB.findAll();
-        assertThat(result).isEqualTo(expResult);
-    }
-
-    @Test
-    public void testNewBook() {
-
+//WHEN
+//THEN
         Book result = instance.create((BookDTO) newBook);
+//VERIFY
         assertThat(result).isEqualTo(newBook);
     }
 
     @Test
-    public void testOne() {
+    public void testList() {
 
+//WHEN
+        List<Book> expResult = FakeDB.findAll();
+//THEN
+        List<Book> result = instance.list();
+//VERIFY
+        assertThat(result).isEqualTo(expResult);
+    }
+
+    @Test
+    public void testRead_OK() {
+
+//WHEN
         Book expResult = FakeDB.findById(TestConstants._TEST_ID2);
+//THEN
         Book result = instance.read(TestConstants._TEST_ID2);
+//VERIFY
         assertThat(result).isEqualTo(expResult);
 
     }
 
-    @Test
-    public void testReplaceBook() {
+    @Test()
+    public void testRead_FAIL() throws URISyntaxException {
 
+//WHEN
+        Book expResult = FakeDB.findById(TestConstants._TEST_ID9);
+        try {
+//THEN
+            Book book = instance.read(TestConstants._TEST_ID9);
+
+//VERIFY
+            assertThat(book).isNotNull();
+            fail("Should newer have come this far...");
+            assertThat(book.getId()).isEqualTo(expResult.getId());
+            assertThat(book.getTitle()).isEqualTo(expResult.getTitle());
+        } catch (BookNotFoundException e) {
+            System.out.println("Error = " + e.getClass());
+        }
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+//WHEN
+//THEN
         Book result = instance.update((BookDTO) newBook, TestConstants._TEST_ID2);
+//VERIFY
         assertThat(result.getId()).isEqualTo(TestConstants._TEST_ID2);
         assertThat(result.getTitle()).isEqualTo(newBook.getTitle());
     }
 
     @Test
-    public void testDeleteBook() {
+    public void testDelete() {
 
+//WHEN
         Book expResult = FakeDB.findById(TestConstants._TEST_ID2);
         assertThat(expResult).isNotNull();
+//THEN
         instance.delete(TestConstants._TEST_ID2);
         expResult = FakeDB.findById(TestConstants._TEST_ID2);
+//VERIFY
         assertThat(expResult).isNull();
 
     }
